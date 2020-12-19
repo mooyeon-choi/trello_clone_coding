284c6d63060d9fb5c0aa0662c1d42c580b5f82ef
mooyeon-choi <dus1208@ajou.ac.kr>
2020-12-19 22:41:57 +0900

    Update List Actions Modal창 요소 밖 클릭시 종료

--- src/components/listSetting.jsx
+++ src/components/listSetting.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 
 class ListSetting extends Component {
+  blockTransmission = event => {
+    event.stopPropagation()
+  }
   render() {
     return (
-      <ul className="list-setting-items">
-        <li className="list-setting-title">List Actions</li>
-        <hr/>
-        <li className="list-setting-item">수정하기</li>
-        <li className="list-setting-item">삭제하기</li>
-      </ul>
+      <div>
+        <ul className="list-setting-items" onClick={this.blockTransmission}>
+          <li className="list-setting-title">List Actions</li>
+          <hr/>
+          <li className="list-setting-item">수정하기</li>
+          <li className="list-setting-item">삭제하기</li>
+        </ul>
+        <div className="list-setting-background"></div>
+      </div>
     );
   }
 }
